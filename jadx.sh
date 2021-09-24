d7230b95732bcab88aef5d01a311e9416ea440bb
wuxianlin <wuxianlinwxl@gmail.com>
2021-09-24 20:53:10 +0800

    vivo iQOO Z1 1.9.3

--- jadx.sh
+++ jadx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-axel -n 10 -o rom.zip https://bigota.d.miui.com/V12.5.4.0.RKMCNXM/miui_ODIN_V12.5.4.0.RKMCNXM_eacb5c9e71_11.0.zip
+axel -n 10 -o rom.zip http://sysupwrdl.vivo.com.cn/upgrade/official/officialFiles/PD1986_A_1.9.3-update-full_1599011925.zip
 
 ./tools/rom.sh rom.zip out
 
